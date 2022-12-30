35632ed5652ba846cacb4b0f86999c409eab9f99
Onur Güneç <ogunec@hotmail.com>
2022-12-30 03:06:23 +0300

    add DefaultController

---
+++ Portfolio.UI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
