81261ce2ec6cef31d0f56bd7c533f0822aaab90a
Onur Güneç <ogunec@hotmail.com>
2022-12-31 02:47:22 +0300

    add featurelist

--- Portfolio.UI/Controllers/DefaultController.cs
+++ Portfolio.UI/Controllers/DefaultController.cs
@@ -17,5 +17,7 @@ namespace Portfolio.UI.Controllers
         {
             return PartialView();
         }
+
+        
     }
 }
---
+++ Portfolio.UI/ViewComponents/FeatureList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.ViewComponents
+{
+    public class FeatureList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
