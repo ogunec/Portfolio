3cd4abad4165fe5a9018c811bc0c5b31b303a379
Onur Güneç <ogunec@hotmail.com>
2023-01-02 04:04:49 +0300

    add PortfoliList component

---
+++ Portfolio.UI/ViewComponents/PortfolioList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.BLL.Abstract;
+
+namespace Portfolio.UI.ViewComponents
+{
+	public class PortfolioList : ViewComponent
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public PortfolioList(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IViewComponentResult Invoke()
+		{
+			var values = _unitOfWork.MyPortfolio.GetAll();
+			return View(values);
+		}
+	}
+}
---
+++ Portfolio.UI/ViewComponents/SkillList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.BLL.Abstract;
+
+namespace Portfolio.UI.ViewComponents
+{
+    public class SkillList : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SkillList(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _unitOfWork.Skill.GetAll();
+            return View(values);
+        }
+    }
+}
