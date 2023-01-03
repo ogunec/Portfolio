f5bb0ddef34c5049aa645f0455833add1268b61a
Onur Güneç <ogunec@hotmail.com>
2023-01-03 19:06:09 +0300

    ExperienceList component added

---
+++ Portfolio.UI/ViewComponents/ExperienceList.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.BLL.Abstract;
+
+namespace Portfolio.UI.ViewComponents
+{
+	public class ExperienceList : ViewComponent
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public ExperienceList(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IViewComponentResult Invoke()
+		{
+			var values = _unitOfWork.Experience.GetAll();
+			return View(values);
+		}
+	}
+}
