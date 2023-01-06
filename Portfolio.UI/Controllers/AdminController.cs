3d5a3ad433241d8a4600f8e8e3a1de2e3ab73732
Onur Güneç <ogunec@hotmail.com>
2023-01-06 03:57:19 +0300

    addAdminController

---
+++ Portfolio.UI/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
