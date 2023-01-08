7a7320a2db159b8cf982c9041bdbae6e3789541d
Onur Güneç <ogunec@hotmail.com>
2023-01-08 23:59:43 +0300

    SkillController added

---
+++ Portfolio.UI/Controllers/SkillController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
