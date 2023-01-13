1d73d8525aba682650ea425100ccf9b3ed94d551
Onur Güneç <ogunec@hotmail.com>
2023-01-13 22:09:06 +0300

    ExperienceController created

---
+++ Portfolio.UI/Controllers/ExperienceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.BLL.Abstract;
+
+namespace Portfolio.UI.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ExperienceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var values = _unitOfWork.Experience.GetAll();
+            return View(values);
+        }
+    }
+}
