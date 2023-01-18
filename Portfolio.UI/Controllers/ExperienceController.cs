06e24c5fbf7f9a8d20ce6382f1bea2c3f4b08abd
Onur Güneç <ogunec@hotmail.com>
2023-01-18 21:31:51 +0300

    Experience CRUD defined

--- Portfolio.UI/Controllers/ExperienceController.cs
+++ Portfolio.UI/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portfolio.BLL.Abstract;
+using Portfolio.MODEL.Concrete;
 
 namespace Portfolio.UI.Controllers
 {
@@ -14,6 +15,42 @@ namespace Portfolio.UI.Controllers
         {
             var values = _unitOfWork.Experience.GetAll();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience exp)
+        {
+            _unitOfWork.Experience.Add(exp);
+            return RedirectToAction("Index","Experience");
+        }
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            Experience selectedExperience = _unitOfWork.Experience.GetById(id);
+            return View(selectedExperience);
+        }
+        [HttpPost]
+        public IActionResult EditExperience(Experience exp)
+        {
+            _unitOfWork.Experience.Update(exp);
+            return RedirectToAction("Index","Experience");
+        }
+        [HttpGet]
+        public IActionResult DeleteExperience(int id)
+        {
+            Experience selectedExperience = _unitOfWork.Experience.GetById(id);
+            return View(selectedExperience);
+        }
+        [HttpPost]
+        public IActionResult DeleteExperience(Experience exp)
+        {
+            _unitOfWork.Experience.Delete(exp);
+            return RedirectToAction("Index", "Experience");
         }
     }
 }
