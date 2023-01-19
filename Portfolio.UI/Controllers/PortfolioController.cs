45f39a7621e6be40791ef0f302a94653d77097c3
Onur Güneç <ogunec@hotmail.com>
2023-01-19 23:48:41 +0300

    PortfolioController created

---
+++ Portfolio.UI/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.BLL.Abstract;
+
+namespace Portfolio.UI.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PortfolioController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var values = _unitOfWork.MyPortfolio.GetAll();
+            return View(values);
+        }
+    }
+}
