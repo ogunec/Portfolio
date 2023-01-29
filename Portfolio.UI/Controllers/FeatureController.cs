69a753fb57fd8a126b263d1fa0a109495f84e94c
Onur Güneç <ogunec@hotmail.com>
2023-01-29 23:58:37 +0300

    FeatureController created

---
+++ Portfolio.UI/Controllers/FeatureController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.BLL.Repository;
+
+namespace Portfolio.UI.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public FeatureController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _unitOfWork.Feature.GetById(1);
+            return View(values);
+        }
+    }
+}
